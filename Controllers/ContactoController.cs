364099e66fd0a52a3dfb5b0fd1d41375608d4c26
ingrid Ochoa <ingrid_ochoa@usmp.pe>
2023-04-21 12:07:25 -0500

    contacto creacion

---
+++ Controllers/ContactoController.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using appPetech.Models;
+using appPetech.Data;
+
+namespace appPetech.Controllers
+{
+    public class ContactoController : Controller
+    {
+        private readonly ILogger<ContactoController> _logger;
+        private readonly ApplicationDbContext _context;
+
+        public ContactoController(ILogger<ContactoController> logger,
+         ApplicationDbContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View("Create");
+        }
+
+        [HttpPost]
+        public IActionResult Create(Contacto objContacto)
+        {
+            _context.Add(objContacto);
+            _context.SaveChanges();
+            ViewData["Message"] = "El contacto ya esta registrado";
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+    }
+}
+
